3d6be3710659d0f140c73418eb41cf57d5237614
Manumi De Silva <manumimihindidesilva@gmail.com>
2025-01-02 21:02:28 +0530

    UI-TID010-Add to cart function

---
+++ src/test/java/starter/stepdefinitions/ui/ProductAvailability.java
@@ -0,0 +1,32 @@
+package starter.stepdefinitions.ui;
+
+import io.cucumber.java.en.*;
+import net.serenitybdd.annotations.Steps;
+import uiTest.pages.HomePage;
+import uiTest.pages.ProductAvailabilityPage;
+import uiTest.pages.ShoppingCartPage;
+
+public class ProductAvailability {
+
+    @Steps
+    HomePage homePage;
+
+    @Steps
+    ShoppingCartPage shoppingCartPage;
+
+    @Steps
+    ProductAvailabilityPage productAvailabilityPage;
+
+    @Then("User should navigate to cart page")
+    public void user_should_navigate_to_cart_page() {productAvailabilityPage.verifyCartPage();}
+
+    @Then("User check total price")
+    public void user_check_total_price() {productAvailabilityPage.verifyPriceWithQuantity();}
+
+    @Given("User check total price of all items")
+    public void user_check_total_price_of_all_items() {productAvailabilityPage.checkTotalPriceOfAllItems();}
+
+    @Given("user delete a item")
+    public void user_delete_a_item() {productAvailabilityPage.clickDeleteButton();}
+
+}
---
+++ src/test/resources/features/search/ui/ui-tid-010-product-availability.feature
@@ -0,0 +1,18 @@
+Feature: Product Availability in Cart
+
+  Scenario: Check available stock
+    Given User should navigate to cart page
+    And User check total price of all items
+    And user delete a item
+
+
+ Scenario: Check item Price
+   Given User should navigate to cart page
+   And User check total price
+
+
+
+
+
+
+
